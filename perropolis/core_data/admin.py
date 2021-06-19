a8a92d548a9f2bae013ce452d1bebdc843bccdf9
data <data.mikava@gmail.com>
2021-06-19 12:39:16 +0400

    listView data vizalization

--- perropolis/core_data/admin.py
+++ perropolis/core_data/admin.py
@@ -6,10 +6,10 @@ from django.contrib import admin
 
 @register(Country)
 class CountryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'created_at')
+    list_display = ('code', 'name', 'phone_number_code', 'created_at')
     list_display_links = list_display
     search_fields = ('name', 'code')
-    sortable_by = ('name', 'created_at')
+    sortable_by = ('code', 'name', 'created_at')
 
 
 class BreedInline(admin.TabularInline):
@@ -28,15 +28,16 @@ class SpecieAdmin(admin.ModelAdmin):
 
 @register(Breed)
 class BreedAdmin(admin.ModelAdmin):
-    list_display = ('name', 'species', 'created_at', 'updated_at')
+    # TODO: species__name to display
+    list_display = ('name', 'species', 'size', 'created_at', 'updated_at')
     list_display_links = list_display
     search_fields = ('name', 'species__name')
-    sortable_by = ('name', 'species__name', 'created_at', 'updated_at')
+    sortable_by = ('name', 'species__name', 'size', 'created_at', 'updated_at')
 
 
 @register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at', 'updated_at')
+    list_display = ('name', 'is_active', 'created_at', 'updated_at')
     list_display_links = list_display
     search_fields = ('name',)
     sortable_by = ('name', 'created_at', 'updated_at')
@@ -58,7 +59,7 @@ class VetSpecialityInline(admin.TabularInline):
 
 @register(Vet)
 class VetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'license_code', 'country')
+    list_display = ('country', 'name', 'license_code', 'receive_emergencies', 'created_at', 'updated_at')
     list_display_links = list_display
     search_fields = ('name','license_code', 'country__name')
     sortable_by = ('name', 'license_code', 'country__name', 'created_at', 'updated_at')
@@ -68,23 +69,23 @@ class VetAdmin(admin.ModelAdmin):
 @register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     # TODO: caledonia API integration!
-    list_display = ('name', 'created_at', 'updated_at')
+    list_display = ('name', 'brand_type', 'created_at', 'updated_at')
     list_display_links = list_display
-    search_fields = ('name',)
-    sortable_by = ('name', 'created_at', 'updated_at')
+    search_fields = ('name','brand_type')
+    sortable_by = ('name', 'brand_type', 'created_at', 'updated_at')
 
 
 @register(PetFood)
 class PetFoodAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at', 'updated_at')
+    list_display = ('name', 'brand', 'created_at', 'updated_at')
     list_display_links = list_display
     search_fields = ('name','brand__name')
-    sortable_by = ('name', 'created_at', 'updated_at')
+    sortable_by = ('name', 'brand__name', 'created_at', 'updated_at')
 
 
 @register(PetDrug)
 class PetDrugAdmin(admin.ModelAdmin):
-    list_display = ('name', 'drug_type', 'brand', 'created_at', 'updated_at')
+    list_display = ('name', 'brand', 'drug_type', 'created_at', 'updated_at')
     list_display_links = list_display
     search_fields = ('name','brand__name', 'drug_type')
     sortable_by = ('name', 'drug_type', 'brand__name', 'created_at', 'updated_at')
