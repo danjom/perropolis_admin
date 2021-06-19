7b478d7b7ae07c9f86cd933a335429e18c982db0
data <data.mikava@gmail.com>
2021-06-19 23:32:28 +0400

    admin user additional fileds

--- perropolis/admin_user/models.py
+++ perropolis/admin_user/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-# Create your models here.
+from django.utils.translation import ugettext_lazy as _
+from core_data.models import Country
 
 
 class User(AbstractUser):
-    pass
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    name = models.CharField(_('Name'), max_length=20)
+    phone_number = models.CharField(_('Phone Number'), max_length=24, null=True, blank=True)
+    created_at = models.DateTimeField(_('Created At'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('Updated At'), auto_now=True)
+    is_blocked = models.BooleanField(default=True)
--- perropolis/perropolis/settings.py
+++ perropolis/perropolis/settings.py
@@ -42,7 +42,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = [
     'guardian',
-    'algoliasearch_django',
+    # 'algoliasearch_django',
     'cloudinary',
 ]
 
