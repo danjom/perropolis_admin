333f00817f97dd1cfe6e2ad1da0b09c70dd7880f
data <data.mikava@gmail.com>
2021-06-21 05:47:52 +0400

    add autocomplete fields in admin

--- perropolis/core_data/admin.py
+++ perropolis/core_data/admin.py
@@ -34,6 +34,7 @@ class BreedAdmin(GuardedModelAdmin):
     list_display_links = list_display
     search_fields = ('name', 'species__name')
     sortable_by = ('name', 'species__name', 'size', 'created_at', 'updated_at')
+    autocomplete_fields = ('species', )
 
 
 @register(Service)
@@ -67,6 +68,7 @@ class VetAdmin(GuardedModelAdmin):
     search_fields = ('name','license_code', 'country__name')
     sortable_by = ('name', 'license_code', 'country__name', 'created_at', 'updated_at')
     inlines = (VetSpecialityInline,)
+    autocomplete_fields = ('country',)
 
 
 @register(Brand)
@@ -98,6 +100,7 @@ class PetFoodAdmin(GuardedModelAdmin):
     list_display_links = list_display
     search_fields = ('name','brand__name')
     sortable_by = ('name', 'brand__name', 'created_at', 'updated_at')
+    autocomplete_fields = ('brand',)
 
 
 @register(PetDrug)
@@ -106,6 +109,7 @@ class PetDrugAdmin(GuardedModelAdmin):
     list_display_links = list_display
     search_fields = ('name','brand__name', 'drug_type')
     sortable_by = ('name', 'drug_type', 'brand__name', 'created_at', 'updated_at')
+    autocomplete_fields = ('brand',)
 
 
 class MedicalActionInline(admin.TabularInline):
