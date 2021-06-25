5538e8c67340f04c63640b01cde4a287eaad18c5
data <data.mikava@gmail.com>
2021-06-25 06:08:10 +0400

    Load environment variables from .env file, located beside manage.py

--- perropolis/perropolis/wsgi.py
+++ perropolis/perropolis/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+from dotenv import load_dotenv
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perropolis.settings')
+
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 application = get_wsgi_application()
